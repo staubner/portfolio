ad475f19131eec608f5694b905928916b3cccdaa
Sean Taubner <sean.taubner@gmail.com>
2022-10-12 18:41:55 -0500

    first significant HTML add, silly js script

---
+++ README.md
@@ -0,0 +1,3 @@
+# Online Portfolio - Sean Taubner
+
+A basic but functional website from which to list and display my development portfolio. HTML, CSS, basic JS.
---
+++ script.js
@@ -0,0 +1,5 @@
+const japanese = document.getElementById('japanese')
+
+japanese.onclick = function () {
+    japanese.innerHTML = 'Japanese 「僕と会話の相手になるのが欲しいの？」'
+}
