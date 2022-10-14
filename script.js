d685d8136754ad453d31b1121d2f30b4b691cd16
Sean Taubner <sean.taubner@gmail.com>
2022-10-14 17:16:29 -0500

    add mixed messages to page and JS file, tweaks to contact page

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const japaneseInterest = document.getElementById('japanese_interest')
-const hyogo = document.getElementById('hyogo')
+const japaneseInterest = document.getElementById('japanese_interest');
+const hyogo = document.getElementById('hyogo');
 
 japaneseInterest.onclick = () => {
     if (japaneseInterest.innerHTML === 'Japanese') {
@@ -16,3 +16,21 @@ hyogo.onclick = () => {
         hyogo.innerHTML = 'Hyogo Performing Arts Center Orchestra'
     }
 }
+
+let mixedMessage = document.getElementById('mixed-message');
+let messageButton = document.getElementById('message-button')
+
+const wordObj = {
+    arr1: ['What has the', 'Hate! Hate! Hate!', 'That is the mark of the Mutant', 'The past', 'If you go looking', 'The gratitude', 'The Emperor\'s'],
+    arr2: ['The Emperor', 'An emotion as pure as it is deep!', 'To be Unclean', 'is always', 'for trouble,', 'of the powerful', 'light'],
+    arr3: ['ever done for me?', 'Let it flow, let it run free!', 'To be Reviled', 'with us.', 'you\'re sure to find it.', 'is a heavy weight to bear.', 'is my torch!']
+}
+
+const phraseGen = () => {
+    let first = wordObj.arr1[Math.floor(Math.random() * wordObj.arr1.length)];
+    let second = wordObj.arr2[Math.floor(Math.random() * wordObj.arr2.length)];
+    let third = wordObj.arr3[Math.floor(Math.random() * wordObj.arr3.length)];
+    mixedMessage.innerHTML = `${first} ${second} ${third}`
+}
+
+messageButton.addEventListener('click', phraseGen)
