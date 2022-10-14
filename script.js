08b2cdaf1b179fa796c97cfa929a8c98b65d57ba
Sean Taubner <sean.taubner@gmail.com>
2022-10-14 18:02:32 -0500

    change mixed messages in script.js

--- script.js
+++ script.js
@@ -24,9 +24,9 @@ resetButton.style.display = 'none'
 mixedMessage.style.display = 'none'
 
 const wordObj = {
-    arr1: ['What has the', 'Hate! Hate! Hate!', 'That is the mark of the Mutant', 'The past', 'If you go looking', 'The gratitude', 'The Emperor\'s'],
-    arr2: ['The Emperor', 'An emotion as pure as it is deep!', 'To be Unclean', 'is always', 'for trouble,', 'of the powerful', 'light'],
-    arr3: ['ever done for me?', 'Let it flow, let it run free!', 'To be Reviled', 'with us.', 'you\'re sure to find it.', 'is a heavy weight to bear.', 'is my torch!']
+    arr1: ['A barking dog', 'Absence makes', 'Doubt is the beginning,', 'Imitation', 'No matter', 'Early to bed, early to rise', 'He who fights and runs away'],
+    arr2: ['never', 'the heart', 'not the end,', 'is the sincerest form', 'where you go,', 'make a man', 'lives to fight'],
+    arr3: ['bites.', 'grow fonder.', 'of wisdom.', 'of flattery.', 'there you are.', 'healthy, wealth, and wise.', 'another day.']
 }
 
 const phraseGen = () => {
